79186bff8f9a8aa460cbad79ec3739c41870361e
MelekTaus Moribundus <melektaus@outlook.at>
2025-04-02 22:43:01 +0200

    (Re)Add .NET Desktop Runtime Requirement

--- Glutspeicher Client/Tausi.NativeWindow/Kernel32.cs
+++ Glutspeicher Client/Tausi.NativeWindow/Kernel32.cs
@@ -6,10 +6,4 @@ public static class Kernel32
 {
     [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     public static extern nint GetModuleHandle([Optional] string lpModuleName);
-
-    [DllImport("kernel32")]
-    public static extern bool AllocConsole();
-
-    [DllImport("kernel32")]
-    public static extern nint GetConsoleWindow();
 }
